ad6f6d2769ab292d9e4e0e3498071676cd6a8357
Georgy Nikitin <georgy_nikitin@epam.com>
2021-03-17 20:09:12 +0300

    Homework-1

---
+++ src/task1_1.ts
@@ -0,0 +1,11 @@
+const LineFeed = '\n'
+
+process.stdin.on('data', data => {
+  const result = reverse(data.toString().trim());
+  process.stdout.write(`${result}${LineFeed.repeat(3)}`);
+});
+
+function reverse(value: string): string {
+  return value.split('').reverse().join('');
+}
+
